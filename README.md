1f2b97086488155bf356d457a251ea627fceb61d
Billy Pamanian <itisbilly@gmail.com>
2025-11-11 02:34:26 -0800

    README update, added SQLite init stuff, working on POST method

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Environment Monitoring System
 
 In 2023 I created a primitive environment monitoring system using esp2388 micro-controllers and a banana pi (Raspberry pis were hard to source then). However, that repo has been lost through computer transitions. This project is a not-so-faithful reconstruction, in that I'm using esp32s (i currently have more on hand), and will be bringing new programming and deployment concepts that I've learned over that period of time.
+
+## TODOs:
+
+* API: GET date ranges
+* API: GET 24 hour highs and lows
+* Integrate an alert system using Pushover, Telegram, or WhatsApp
--- back-end/controllerFunctions/index.ts
+++ back-end/controllerFunctions/index.ts
@@ -3,9 +3,47 @@ import { Context } from "hono";
 
 const db = new DataBase.DatabaseSync("db.sqlite3");
 
-// Define DB init here
-// endif
+// Init DB
+db.exec(`
+  CREATE TABLE IF NOT EXISTS buildings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+  );
+`);
 
+db.exec(`
+  CREATE TABLE IF NOT EXISTS devices (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    device_id TEXT NOT NULL
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS sensors (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    building INTEGER,
+    device TEXT NOT NULL,
+    sensor_id INTEGER,
+    FOREIGN KEY (building) REFERENCES buildings(id),
+    FOREIGN KEY (device) REFERENCES devices(id)
+  );
+`);
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS readings (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    sensor TEXT NOT NULL,
+    time INTEGER,
+    temperature REAL,
+    hunidity REAL,
+    vpd REAL,
+    FOREIGN KEY (sensor) REFERENCES sensors(id)
+  );
+`);
+
+/**
+ * Front end goes here
+ */
 export const get_controllerIndex = (c: Context): Response => {
   return c.text("Front end goes here", 200);
 };
@@ -13,10 +51,12 @@ export const get_controllerIndex = (c: Context): Response => {
 /**
  * Takes request body, parses and adds to database
  */
-export const post_inserData = async (c: Context): Promise<Response> => {
+export const post_insertReadings = async (c: Context): Promise<Response> => {
   let errorFlag = true;
+  // TODO: SQL syntax: device should select `devices` member based on device_id
+  // TODO: Ignore if exists, insert device_id
   const db_insertPrepare = db.prepare(`
-    INSERT INTO readings (device_id, name, temp, humid, time) VALUES (?, ?, ?, ?, ?);
+    INSERT INTO readings (device, sensor, temperature, humidity, time, vpd) VALUES (?, ?, ?, ?, ?);
   `);
 
   try {
@@ -25,12 +65,18 @@ export const post_inserData = async (c: Context): Promise<Response> => {
 
     // Insert into DB
     for (const i in formatted.data) {
+      const vpd = getVPD(
+        formatted.data[i].temperature,
+        formatted.data[i].humidity,
+      );
+
       db_insertPrepare.run(
         formatted.device_id,
         formatted.data[i].name,
         formatted.data[i].temperature,
         formatted.data[i].humidity,
         formatted.date,
+        vpd,
       );
     }
 
@@ -46,5 +92,17 @@ export const post_inserData = async (c: Context): Promise<Response> => {
 
 export default {
   get_controllerIndex,
-  post_inserData,
+  post_insertReadings,
 };
+
+/**
+ * Calculates VPD
+ * @param {number} temperature - Temp in F°
+ * @param {number} humidity
+ */
+function getVPD(temperature: number, humidity: number): number {
+  const c = (temperature - 32) / 1.8;
+  const svp = 0.61078 * Math.E ** ((17.27 * c) / (c + 237.3)); // Tetens equation
+  const vpd = svp * (1 - humidity / 100);
+  return vpd;
+}
